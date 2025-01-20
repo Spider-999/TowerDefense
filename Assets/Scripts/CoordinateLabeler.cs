d0d6676fdb1c251fba96dde41f43d5650b1367c6
Patrascu Hary <harypatrascu.1@gmail.com>
2025-01-20 14:26:10 +0200

    Coordinate labeler

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+// This makes sure the script runs always, even in edit mode.
+[ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshPro _label;
+    private Vector2Int _position = new Vector2Int();
+
+    // Awake is the first function called in Unity.
+    private void Awake()
     {
-        
+        _label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateTileName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        _position.x = Mathf.RoundToInt(transform.parent.position.x /
+                                       UnityEditor.EditorSnapSettings.move.x);
+        _position.y = Mathf.RoundToInt(transform.parent.position.z /
+                                       UnityEditor.EditorSnapSettings.move.z);
+
+        _label.text = $"({_position.x},{_position.y})";
+    }
+
+    void UpdateTileName()
+    {
+        transform.parent.name = _position.ToString();
     }
 }
