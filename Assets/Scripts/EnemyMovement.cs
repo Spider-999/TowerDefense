01316a682dfdb08a597a32d8b4c2f2057bd2185a
Patrascu Hary <harypatrascu.1@gmail.com>
2025-01-20 17:37:24 +0200

    Enemy follows roads

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<Road> _roads = new List<Road>();
+    [SerializeField] float waitTime = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintRoads();
+        StartCoroutine(FollowRoad());
     }
 
-    void PrintRoads()
+    // Coroutine
+    IEnumerator FollowRoad()
     {
         foreach (Road road in _roads)
-            Debug.Log(road.name);
+        {
+            // Set the position of the enemy to the position of
+            // the current road in the foreach loop
+            transform.position = road.transform.position;
+
+            // Go back to Start for one sec, then come back in the
+            // loop and print the next road name
+            yield return new WaitForSeconds(waitTime);
+        }
     }
 
 }
