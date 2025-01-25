1a57b63a6d134d6f71126f77bff3af91e920deaf
Patrascu Hary <harypatrascu.1@gmail.com>
2025-01-26 00:29:17 +0200

    The enemies have their road built automatically when they spawn now by getting the tag of the parent road object and storing all of the roads as children of that parent object.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Road> _roads = new List<Road>();
-    [SerializeField] [Range(1.0f, 5.0f)] float enemySpeed = 1.0f;
+    [SerializeField] private List<Road> _roads = new List<Road>();
+    [SerializeField] [Range(1.0f, 5.0f)] private float _enemySpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        FindAndBuildPath();
         StartCoroutine(FollowRoad());
     }
 
-    // Coroutine
-    IEnumerator FollowRoad()
+    void FindAndBuildPath()
+    {
+        // Clear the existing roads
+        _roads.Clear();
+
+        // Get the parent of object of the roads with the tag "Road"
+        GameObject parentRoad = GameObject.FindGameObjectWithTag("Road");
+
+        // Loop through all the children of the parentRoad
+        // and add them to the _roads list to build the level's road.
+        foreach (Transform road in parentRoad.transform)
+            _roads.Add(road.GetComponent<Road>());
+    }
+
+        // Coroutine
+        IEnumerator FollowRoad()
     {
         foreach (Road road in _roads)
         {
@@ -35,7 +51,7 @@ public class EnemyMovement : MonoBehaviour
             {
                 // Every frame we update the time to travel between
                 // the start position and end position
-                time += Time.deltaTime * enemySpeed;
+                time += Time.deltaTime * _enemySpeed;
                 transform.position = Vector3.Lerp(startPos, endPos, time);
 
                 yield return new WaitForEndOfFrame();
